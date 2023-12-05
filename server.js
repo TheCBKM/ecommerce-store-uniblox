9db4f171073bb550a72ceb2ae92ee827face21e2
thecbkm <rjoshi14899@gmail.com>
2023-12-05 17:30:54 +0530

    Add server.js with basic express setup and listen on port 3000

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+
+
+const port = 3000;
+
+const n = 3; // The interval for providing discount coupons
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+  
