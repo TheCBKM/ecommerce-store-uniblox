dfc706fb6469769bfd081f1d8778c690222d957a
thecbkm <rjoshi14899@gmail.com>
2023-12-05 17:33:24 +0530

    Add in-memory store and initialize variables for items and discounts

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ const port = 3000;
 
 const n = 3; // The interval for providing discount coupons
 
+// In-memory store
+let users = {}; // Stores user data
+let items = [
+  // List of available items
+  { id: 1, name: "Item 1", price: 10 },
+  { id: 2, name: "Item 2", price: 50 },
+];
+
+let itemsPurchasedCount = 0; // Total number of items purchased
+let totalPurchaseAmount = 0; // Total amount from all purchases
+let discountAmount = 0; // Total discount amount given
+let discountList = []; // List of all discount coupons
 
 // Start the server
 app.listen(port, () => {
