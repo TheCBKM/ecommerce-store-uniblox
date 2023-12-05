0e2c525ef0c3b6638f5753eb7e186ad22754ec2c
thecbkm <rjoshi14899@gmail.com>
2023-12-06 00:41:45 +0530

    Add new items to the list of available items

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ let items = [
   // List of available items
   { id: 1, name: "Item 1", price: 10 },
   { id: 2, name: "Item 2", price: 50 },
+  { id: 3, name: "Item 3", price: 15 },
+  { id: 4, name: "Item 4", price: 25 },
+  { id: 5, name: "Item 5", price: 30 },
+  { id: 6, name: "Item 6", price: 45 },
+  { id: 7, name: "Item 7", price: 35 },
+  { id: 8, name: "Item 8", price: 20 },
+  { id: 9, name: "Item 9", price: 55 },
+  { id: 10, name: "Item 10", price: 40 },
+  { id: 11, name: "Item 11", price: 60 },
 ];
 let itemsPurchasedCount = 0; // Total number of items purchased
 let totalPurchaseAmount = 0; // Total amount from all purchases
@@ -149,6 +158,5 @@ app.get("/admin/stats/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
 
 module.exports = app;
